bd3a8dae8901b82a23528d56bd0c00db2c64d6b9
simply-anshul <anshul.surpaithankar.1@gmail.com>
2023-08-12 01:09:33 +0530

    Initial Commit

---
+++ src/main/java/com/anshul/expensive/ExpensiveApplication.java
@@ -0,0 +1,13 @@
+package com.anshul.expensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshul/expensive/ExpensiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshul.expensive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
