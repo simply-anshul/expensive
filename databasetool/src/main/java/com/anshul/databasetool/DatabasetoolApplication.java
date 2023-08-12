744836a5bf1884d76d006c82646a78dee9534403
simply-anshul <anshul.surpaithankar.1@gmail.com>
2023-08-12 10:30:37 +0530

    Change in project structure

---
+++ databasetool/src/main/java/com/anshul/databasetool/DatabasetoolApplication.java
@@ -0,0 +1,13 @@
+package com.anshul.databasetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasetoolApplication.class, args);
+	}
+
+}
